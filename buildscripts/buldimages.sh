94642b9cc86545665a8e9079413c3bc7224d112b
spara <spara@github.com>
2018-05-14 13:20:53 -0700

    removed js client

--- buildscripts/buldimages.sh
+++ buildscripts/buldimages.sh
@@ -9,9 +9,6 @@ docker image build -t $DTR_HOST/frontend/java_web:1 .
 cd $repo_path/task_3/java_app 
 docker image build -t $DTR_HOST/frontend/java_web:2 .
 
-cd $repo_path/task_4/signup_client
-docker image build -t $DTR_HOST/frontend/signup_client .
-
 cd $repo_path/task_2/database
 docker image build -t $DTR_HOST/backend/database .
 
@@ -29,7 +26,6 @@ docker image build -t $DTR_HOST/backend/worker:2 .
 docker login -u frontend_user -p user1234 $DTR_HOST
 docker image push $DTR_HOST/frontend/java_web:1
 docker image push $DTR_HOST/frontend/java_web:2
-docker image push $DTR_HOST/frontend/signup_client
 
 docker login -u backend_user -p user1234 $DTR_HOST
 docker image push $DTR_HOST/backend/database
